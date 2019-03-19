e87dea0b664f97f579dc0e0db3fd1e02075067fc
Jonas Gebele <jonas.gebele@solve-it-mvi.com>
2019-03-19 14:17:57 +0100

    included targets

---
+++ target/generated-sources/annotations/com/codebind/Game_.java
@@ -0,0 +1,15 @@
+package com.codebind;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Game.class)
+public abstract class Game_ {
+
+	public static volatile SingularAttribute<Game, Long> id;
+	public static volatile SingularAttribute<Game, String> title;
+
+}
+
